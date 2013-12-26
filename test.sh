6b029fd6bcc1db01d83ac6c3208499a0a2555d30
Elijah Buck <elijah.buck@gmail.com>
2013-12-26 11:26:42 -0500

    updated test.sh uses ignore_inode_dev option

--- test.sh
+++ test.sh
@@ -102,3 +102,50 @@ pkill -f /usr/bin/ruby.*test.rb
 cat $SINCEDB_PATH | sed 's/^/    /'
 
 echo "TEST 2: DONE"
+
+##TEST3
+echo "TEST 3: remounting results in missing lines"
+#Because major/minor of inode changes depending on mount order of different servers
+rm -f $SINCEDB_PATH
+umount ./mnt/a ; umount ./mnt/b
+mount.cifs $SHAREA ./mnt/a -o user=$USER,domain=$DOMAIN
+mount.cifs $SHAREB ./mnt/b -o user=$USER,domain=$DOMAIN
+cat <<EOF >test.rb
+require "rubygems"
+require "filewatch/tail"
+t = FileWatch::Tail.new( :ignore_inode_dev => true )
+t.tail("$MYROOT/mnt/*/*.txt")
+t.subscribe do |path, line|
+  puts "#{path}: #{line}"
+end
+EOF
+
+echo a > ./mnt/a/1a.txt ; echo b > ./mnt/b/1b.txt
+ruby ./test.rb &
+sleep 5
+echo aa >> ./mnt/a/1a.txt ; sleep 5
+echo bb >> ./mnt/b/1b.txt ; sleep 5
+echo bbb >> ./mnt/b/1b.txt ; sleep 5
+echo aaa >> ./mnt/a/1a.txt ; sleep 5
+sleep 15 
+pkill -f /usr/bin/ruby.*test.rb
+cat $SINCEDB_PATH | sed 's/^/    /'
+
+echo "restarting tail with no file changes (should be no output)"
+sleep 5 ; ruby ./test.rb &
+sleep 5
+pkill -f /usr/bin/ruby.*test.rb
+cat $SINCEDB_PATH | sed 's/^/    /' 
+
+echo "restarting tail after remounting in different order, with file changes"
+umount ./mnt/a ; umount ./mnt/b
+mount.cifs $SHAREB ./mnt/b -o user=$USER,domain=$DOMAIN
+mount.cifs $SHAREA ./mnt/a -o user=$USER,domain=$DOMAIN
+echo REMOUNTED >> ./mnt/b/1b.txt
+sleep 5 ; ruby ./test.rb &
+sleep 5
+pkill -f /usr/bin/ruby.*test.rb
+cat $SINCEDB_PATH | sed 's/^/    /'
+
+echo "TEST 3: DONE"
+
